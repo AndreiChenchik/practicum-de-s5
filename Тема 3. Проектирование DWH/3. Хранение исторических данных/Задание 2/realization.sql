c34797a08c8feba51a4d8e7e1b4bb01599774d2a
Andrei Chenchik <andrei@chenchik.me>
2022-06-22 20:58:35 +0200

    3.3.2 wip

--- Тема 3. Проектирование DWH/3. Хранение исторических данных/Задание 2/realization.sql
+++ Тема 3. Проектирование DWH/3. Хранение исторических данных/Задание 2/realization.sql
@@ -1,20 +1,25 @@
 -- Удалите внешний ключ из sales
-...
+alter table sales
+    drop constraint sales_products_product_id_fk;
 
 -- Удалите первичный ключ из products
-...
+alter table products 
+    drop constraint products_pk,
 
 -- Добавьте новое поле id для суррогантного ключа в products
-...
+    add column id serial not null,
 
 -- Сделайте данное поле первичным ключом
-...
+    add constraint products_pk primary key (id),
 
 -- Добавьте дату начала действия записи в products
-...
+    add column valid_from timestamptz,
 
 -- Добавьте дату окончания действия записи в products
-...
+    add column valid_to timestamptz;
 
 -- Добавьте новый внешний ключ sales_products_id_fk в sales
-...
+alter table sales
+    add constraint sales_products_id_fk references products (id);
+
+--
