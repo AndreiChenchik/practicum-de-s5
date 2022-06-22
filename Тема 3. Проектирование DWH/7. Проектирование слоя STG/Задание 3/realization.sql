f5e09f74b362bb0c3e823dce95cfffaa8948609a
Andrei Chenchik <andrei@chenchik.me>
2022-06-22 20:06:42 +0200

    3.7.3

---
+++ Тема 3. Проектирование DWH/7. Проектирование слоя STG/Задание 3/realization.sql
@@ -0,0 +1,25 @@
+drop table if exists stg.ordersystem_users;
+CREATE TABLE stg.ordersystem_users
+(
+    id serial CONSTRAINT ordersystem_users_pkey PRIMARY KEY,
+    object_id varchar NOT NULL CONSTRAINT ordersystem_users_object_id_uindex UNIQUE,
+    object_value text NOT NULL
+); 
+
+drop table if exists stg.ordersystem_restaurants;
+CREATE TABLE stg.ordersystem_restaurants
+(
+    id serial CONSTRAINT ordersystem_restaurants_pkey PRIMARY KEY,
+    object_id varchar NOT NULL CONSTRAINT ordersystem_restaurants_object_id_uindex UNIQUE,
+    object_value text NOT NULL
+); 
+
+drop table if exists stg.ordersystem_orders;
+CREATE TABLE stg.ordersystem_orders
+(
+    id serial CONSTRAINT ordersystem_orders_pkey PRIMARY KEY,
+    object_id varchar NOT NULL CONSTRAINT ordersystem_orders_object_id_uindex UNIQUE,
+    object_value text NOT NULL
+); 
+
+-- Двигайтесь дальше! Ваш код: Ve7J48uY2K
