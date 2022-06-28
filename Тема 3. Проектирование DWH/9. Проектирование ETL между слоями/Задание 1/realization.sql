30439a77e8489ca4d1ec3b923fe4ad293d716db7
Andrei Chenchik <andrei@chenchik.me>
2022-06-28 16:59:29 +0200

    3.9.1

--- Тема 3. Проектирование DWH/9. Проектирование ETL между слоями/Задание 1/realization.sql
+++ Тема 3. Проектирование DWH/9. Проектирование ETL между слоями/Задание 1/realization.sql
@@ -1,3 +1,84 @@
+drop schema if exists cdm cascade;
+create schema cdm;
+
+create table if not exists cdm.dm_settlement_report (
+    id serial not null,
+    restaurant_id integer not null,
+    restaurant_name varchar not null,
+    settlement_date date not null check(settlement_date > '2022-01-01' AND settlement_date < '2050-01-01'),
+    settlement_year smallint not null,
+    settlement_month smallint not null,
+    orders_count integer not null,
+    orders_total_sum numeric(14, 2) not null,
+    orders_bonus_payment_sum numeric(14, 2) not null,
+    orders_bonus_granted_sum numeric(14, 2) not null,
+    order_processing_fee numeric(14, 2) not null,
+    restaurant_reward_sum numeric(14, 2) not null
+);
+
+alter table cdm.dm_settlement_report
+    drop constraint if exists pk_id,
+    add constraint pk_id primary key (id),   
+
+    alter column orders_count drop default,
+    alter column orders_count set default 0,
+    drop constraint if exists orders_count_range,
+    add constraint orders_count_range check (
+        orders_count >= 0
+    ),
+    
+    alter column orders_total_sum drop default,
+    alter column orders_total_sum set default 0,
+    drop constraint if exists orders_total_sum_range,
+    add constraint orders_total_sum_range check (
+        orders_total_sum >= 0
+    ),
+
+    alter column orders_bonus_payment_sum drop default,
+    alter column orders_bonus_payment_sum set default 0,
+    drop constraint if exists orders_bonus_payment_sum_range,
+    add constraint orders_bonus_payment_sum_range check (
+        orders_bonus_payment_sum >= 0
+    ),
+
+    alter column orders_bonus_granted_sum drop default,
+    alter column orders_bonus_granted_sum set default 0,
+    drop constraint if exists orders_bonus_granted_sum_range,
+    add constraint orders_bonus_granted_sum_range check (
+        orders_bonus_granted_sum >= 0
+    ),
+
+    alter column restaurant_reward_sum drop default,
+    alter column restaurant_reward_sum set default 0,
+    drop constraint if exists restaurant_reward_sum_range,
+    add constraint restaurant_reward_sum_range check (
+        restaurant_reward_sum >= 0
+    ),
+
+    alter column order_processing_fee drop default,
+    alter column order_processing_fee set default 0,
+    drop constraint if exists order_processing_fee_range,
+    add constraint order_processing_fee_range check (
+        order_processing_fee >= 0
+    ),
+
+    drop constraint if exists settlement_year_range,
+    add constraint settlement_year_range check ( 
+        settlement_year >= 2022 and settlement_year < 2500
+    ),
+
+    drop constraint if exists settlement_month_range,
+    add constraint settlement_month_range check (
+        settlement_month >= 1 and settlement_month <= 12
+    ),
+
+    drop constraint if exists unique_rid_syear_smonth,
+    add constraint unique_rid_syear_smonth unique (
+        restaurant_id, settlement_date, settlement_year, settlement_month
+    )
+;
+
+
 DELETE FROM dds.dm_products;
 DELETE FROM dds.dm_orders;
 DELETE FROM dds.dm_users;
@@ -15,7 +96,8 @@ COPY dds.fct_product_sales (id, product_id, order_id, count, price, total_sum, b
 
 insert into cdm.dm_settlement_report (
     restaurant_id,
-    restaurant_name, 
+    restaurant_name,
+    settlement_date, 
     settlement_year,
     settlement_month,
     orders_count,
@@ -27,10 +109,11 @@ insert into cdm.dm_settlement_report (
 )
     select
         dmr.id                                              as restaurant_id,
-        dmr.restaurant_name                                 as restaurant_name, 
+        dmr.restaurant_name                                 as restaurant_name,
+        dmt.date                                            as settlement_date, 
         dmt.year                                            as settlement_year,
         dmt.month                                           as settlement_month,
-        count(distinct fps.order_id)                        as orders_count,
+        sum(fps.count)                                      as orders_count,
         sum(fps.total_sum)                                  as orders_total_sum,
         sum(fps.bonus_payment)                              as orders_bonus_payment_sum,
         sum(fps.bonus_grant)                                as orders_bonus_granted_sum,
@@ -48,9 +131,10 @@ insert into cdm.dm_settlement_report (
         dmr.restaurant_name,
         dmr.id,
         dmt.year,
-        dmt.month
+        dmt.month,
+        dmt.date
 on conflict 
-    (restaurant_id, settlement_year, settlement_month)
+    (restaurant_id, settlement_date, settlement_year, settlement_month)
     do update set (
         orders_count,
         orders_total_sum,
@@ -67,4 +151,4 @@ on conflict
         excluded.restaurant_reward_sum
     )
     
--- 
+-- Двигайтесь дальше! Ваш код: mgXgcqQzFv
