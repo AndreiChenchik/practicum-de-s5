5b418402a9017cb397417bbfa9cdfe9c62db0b12
Andrei Chenchik <andrei@chenchik.me>
2022-06-28 08:40:06 +0200

    full update for stg layer

--- Тема 3. Проектирование DWH/6. Проектирование слоя STG/Задание 3/realization.sql
+++ Тема 3. Проектирование DWH/6. Проектирование слоя STG/Задание 3/realization.sql
@@ -1,3 +1,47 @@
+drop schema if exists stg cascade;
+create schema stg;
+
+CREATE TABLE
+    stg.bonussystem_users (
+        id integer NOT NULL,
+        order_user_id text NOT NULL
+    );
+
+ALTER TABLE
+    stg.bonussystem_users
+ADD
+    CONSTRAINT users_pkey PRIMARY KEY (id);
+
+CREATE TABLE
+    stg.bonussystem_ranks (
+        id integer NOT NULL,
+        name character varying(2048) NOT NULL,
+        bonus_percent numeric(19, 5) NOT NULL,
+        min_payment_threshold numeric(19, 5) NOT NULL
+    );
+
+ALTER TABLE
+    stg.bonussystem_ranks
+ADD
+    CONSTRAINT ranks_pkey PRIMARY KEY (id);
+
+
+CREATE TABLE
+    stg.bonussystem_events (
+        id integer NOT NULL,
+        event_ts timestamp without time zone NOT NULL,
+        event_type character varying NOT NULL,
+        event_value text NOT NULL
+    );
+
+ALTER TABLE
+    stg.bonussystem_events
+ADD
+    CONSTRAINT bonussystem_events_pkey PRIMARY KEY (id);
+
+create index idx_bonussystem_events__event_ts 
+    ON stg.bonussystem_events (event_ts);
+
 drop table if exists stg.ordersystem_users;
 CREATE TABLE stg.ordersystem_users
 (
