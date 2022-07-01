24ee79453bf5885438a76458abcef4103de63b04
Andrei Chenchik <andrei@chenchik.me>
2022-07-01 11:04:03 +0200

    fix old tasks for new db structure

--- Тема 3. Проектирование DWH/6. Проектирование слоя STG/Задание 2/realization.sql
+++ Тема 3. Проектирование DWH/6. Проектирование слоя STG/Задание 2/realization.sql
@@ -3,7 +3,8 @@ CREATE TABLE stg.ordersystem_users
 (
     id serial CONSTRAINT ordersystem_users_pkey PRIMARY KEY,
     object_id varchar NOT NULL CONSTRAINT ordersystem_users_object_id_key UNIQUE,
-    object_value text NOT NULL
+    object_value text NOT NULL,
+    update_ts timestamp not null
 ); 
 
 drop table if exists stg.ordersystem_restaurants;
@@ -11,7 +12,8 @@ CREATE TABLE stg.ordersystem_restaurants
 (
     id serial CONSTRAINT ordersystem_restaurants_pkey PRIMARY KEY,
     object_id varchar NOT NULL CONSTRAINT ordersystem_restaurants_object_id_key UNIQUE,
-    object_value text NOT NULL
+    object_value text NOT NULL,
+    update_ts timestamp not null
 ); 
 
 drop table if exists stg.ordersystem_orders;
@@ -19,7 +21,8 @@ CREATE TABLE stg.ordersystem_orders
 (
     id serial CONSTRAINT ordersystem_orders_pkey PRIMARY KEY,
     object_id varchar NOT NULL CONSTRAINT ordersystem_orders_object_id_key UNIQUE,
-    object_value text NOT NULL
+    object_value text NOT NULL,
+    update_ts timestamp not null
 ); 
 
 -- Двигайтесь дальше! Ваш код: OIoYDT7RQC
--- Тема 3. Проектирование DWH/6. Проектирование слоя STG/Задание 3/realization.sql
+++ Тема 3. Проектирование DWH/6. Проектирование слоя STG/Задание 3/realization.sql
@@ -47,7 +47,8 @@ CREATE TABLE stg.ordersystem_users
 (
     id serial CONSTRAINT ordersystem_users_pkey PRIMARY KEY,
     object_id varchar NOT NULL CONSTRAINT ordersystem_users_object_id_uindex UNIQUE,
-    object_value text NOT NULL
+    object_value text NOT NULL,
+    update_ts timestamp not null
 ); 
 
 drop table if exists stg.ordersystem_restaurants;
@@ -55,7 +56,8 @@ CREATE TABLE stg.ordersystem_restaurants
 (
     id serial CONSTRAINT ordersystem_restaurants_pkey PRIMARY KEY,
     object_id varchar NOT NULL CONSTRAINT ordersystem_restaurants_object_id_uindex UNIQUE,
-    object_value text NOT NULL
+    object_value text NOT NULL,
+    update_ts timestamp not null
 ); 
 
 drop table if exists stg.ordersystem_orders;
@@ -63,7 +65,8 @@ CREATE TABLE stg.ordersystem_orders
 (
     id serial CONSTRAINT ordersystem_orders_pkey PRIMARY KEY,
     object_id varchar NOT NULL CONSTRAINT ordersystem_orders_object_id_uindex UNIQUE,
-    object_value text NOT NULL
+    object_value text NOT NULL,
+    update_ts timestamp not null
 ); 
 
 -- Двигайтесь дальше! Ваш код: Ve7J48uY2K
